31b5347e9c846e16885810eb9aafb6b6599c676e
Alexander Lichter <manniL@gmx.net>
2018-10-20 12:56:53 +0100

    refactor: use extendBuild API instead of custom chaining

--- test/fixture/modules/error.js
+++ test/fixture/modules/error.js
@@ -1,11 +1,6 @@
 module.exports = function () {
-  const oldExtendFunction = this.options.build.extend
-
-  this.options.build.extend = (config, options) => {
+  this.extendBuild((config, options) => {
     const imageLoaderRule = config.module.rules.find(rule => rule.test && /svg/.test(rule.test.toString()))
     imageLoaderRule.test = /^$/
-    if (oldExtendFunction) {
-      oldExtendFunction(config, options)
-    }
-  }
+  })
 }
