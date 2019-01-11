e7928ab7e11c96c6838f44137b0f232509f1a301
Alexander Lichter <manniL@gmx.net>
2019-01-11 19:40:24 +0000

    fix: re-enable lazy-loading capabilities (#28)
    This has been fixed by the latest svg-to-vue-component version

---
+++ test/fixture/pages/two.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nuxt-logo viewBox="1 2 3 4" width="100" />
+    <nuxt-logo-two />
+  </div>
+</template>
+
+<script>
+export default {
+  loading: false,
+  components: {
+    NuxtLogo: () => import('../components/Nuxt.svg'),
+    NuxtLogoTwo: () => import('../components/NuxtTwo.svg')
+  }
+}
+</script>
