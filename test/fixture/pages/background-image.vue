9df630024f20f07b7ec302169617976a77ffa67a
Ahmad Mahmoud <a.3b3ziz@gmail.com>
2020-01-22 16:44:12 +0200

    feat: proper loading svg used as background url (#50)

---
+++ test/fixture/pages/background-image.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>-</h1>
+    <h2>-</h2>
+  </div>
+</template>
+
+<script>
+import '../modules/main.css'
+export default {
+  loading: false,
+}
+</script>
+
+<style>
+h1 {
+  background: url('../components/NuxtTwo.svg?inline')
+}
+</style>
+
